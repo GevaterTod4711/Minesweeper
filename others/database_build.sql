d1e2916ae126da44f6247d43eb5d35901792ddf7
Tobias Faßbender <tobias-fassbender@gmx.de>
2016-12-15 16:38:56 +0100

    new database script; working on Database class; working on login.php

--- others/database_build.sql
+++ others/database_build.sql
@@ -2,30 +2,33 @@ CREATE DATABASE IF NOT EXISTS minesweeper;
 
 --erst datenbank anlegen und auswählen; dann andere befehle
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS minesweeper.users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(25),
 	email VARCHAR(50),
 	pwd TEXT
 	);
 	
-CREATE TABLE IF NOT EXISTS highscore (
+CREATE TABLE IF NOT EXISTS minesweeper.highscore (
 	id INT REFERENCES user.id,
 	score INT
 	);
 	
-CREATE TABLE IF NOT EXISTS achievements_all (
+CREATE TABLE IF NOT EXISTS minesweeper.achievements_all (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(25),
 	description TEXT
 	);
 
-CREATE TABLE IF NOT EXISTS achievements_user (
+CREATE TABLE IF NOT EXISTS minesweeper.achievements_user (
 	user_id INT REFERENCES user.id,
 	achievement_id INT REFERENCES achievements_all.id
 	);
 	
-CREATE TABLE IF NOT EXISTS messages (
+CREATE TABLE IF NOT EXISTS minesweeper.messages (
 	from_id INT REFERENCES user.id,
 	message TEXT
 	);
+
+CREATE USER minesweeper_user IDENTIFIED BY 'asdf';
+GRANT ALL ON minesweeper.* TO minesweeper_user;
