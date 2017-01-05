88b999d1a81615b14b84c0f960a87b83f6afd5a5
oliver <oliver@wlabs.de>
2017-01-05 13:13:39 +0100

    remove return statement

--- register.php
+++ register.php
@@ -3,12 +3,13 @@
 require 'bootstrap.php';
 
 require 'lib/Database.class.php';
+
 $name = filter_input(INPUT_POST,'user',FILTER_SANITIZE_SPECIAL_CHARS);
 $passwort = filter_input(INPUT_POST, 'pw', FILTER_UNSAFE_RAW);
 
 $hash = crypt($passwort);
 $dbh = Database::getInstance();
-$return = $dbh->registerUser($name,$hash);
+//$return = $dbh->registerUser($name,$hash);
 
 $template = $twig->load('register.tpl.html');
 
