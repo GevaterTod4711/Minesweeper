3e5996a901de5b40a5b5def971e08986e8d1be7a
Artur Weigandt <Art4@users.noreply.github.com>
2017-01-05 15:38:05 +0100

    Reload Twig cache

--- bootstrap.php
+++ bootstrap.php
@@ -8,5 +8,5 @@ $loader = new Twig_Loader_Filesystem('templates');
 
 $twig = new Twig_Environment($loader, array(
     'cache' => 'cache'.DIRECTORY_SEPARATOR.'twig',
-    'autoload' => true,
+    'auto_reload' => true,
 ));
