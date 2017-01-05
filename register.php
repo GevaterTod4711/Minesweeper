3693305771cbc0c11648a24b0ddd74c663a393fb
oliver <oliver@wlabs.de>
2017-01-05 12:53:04 +0100

    merge conflicts

--- register.php
+++ register.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 require 'lib/Database.class.php';
 $name = filter_input(INPUT_POST,'user',FILTER_SANITIZE_SPECIAL_CHARS);
 $passwort = filter_input(INPUT_POST, 'pw', FILTER_UNSAFE_RAW);
@@ -7,7 +6,6 @@ $passwort = filter_input(INPUT_POST, 'pw', FILTER_UNSAFE_RAW);
 $hash = crypt($passwort);
 $dbh = Database::getInstance();
 $return = $dbh->registerUser($name,$hash);
-=======
 
 session_start();
 
@@ -16,5 +14,4 @@ require 'lib/Template.class.php';
 $tpl = new Template();
 $tpl->assign('{$title}', 'Register');
 $tpl->display('templates/register.tpl.html');
->>>>>>> 61c017caae61989692b84f739cac7d1e40e8239f
 ?>
