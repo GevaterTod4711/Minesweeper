7dcd22de68516c80e605ccaeba53103f496bc064
Tobias Faßbender <tobias-fassbender@gmx.de>
2017-01-12 16:26:10 +0100

    new register and login

--- public/register.php
+++ public/register.php
@@ -5,9 +5,10 @@ require '../bootstrap.php';
 $name = filter_input(INPUT_POST,'user',FILTER_SANITIZE_SPECIAL_CHARS);
 $passwort = filter_input(INPUT_POST, 'pw', FILTER_UNSAFE_RAW);
 
-$hash = crypt($passwort);
+//$hash = crypt($passwort);
 $dbh = Database::getInstance();
-//$return = $dbh->registerUser($name,$hash);
+$hash = $dbh->getPasswordHash($password);
+$return = $dbh->registerUser($name,$hash);
 
 $template = $twig->load('register.tpl.html');
 
